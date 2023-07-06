533496bff7234909b3a7bd457a2318793eb80ad8
vignesh-ds <vignesh.kumar21@yahoo.com>
2023-07-06 18:57:12 +0530

    code share

---
+++ file_1.py
@@ -0,0 +1,34 @@
+# 1.Print
+# print("hello world")
+
+# 2.single line comment loop
+# multiple line comment
+# x ='vignesh'
+# if x == 'sarah': # inline comment checking the match
+#     print("match")
+# else:
+#     print("no match")
+    
+# 3. variable assign
+# a,b,c = 15,20,25
+# print(a)
+# print(b) 
+# print(c)   
+# del c
+
+# 4. datatypes
+# a = 5
+# b = 6.5
+# c = 'vignesh'
+# d = ['1','3','vignesh']
+# e = {a:'37',b:'40'}
+# f = {1,2,3}
+# g = (1,2,3)
+
+# print(type(a))
+# print(type(b)) 
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
